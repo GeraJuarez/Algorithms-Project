87f5de47eb1c5f1ffbcc2d22cad1da2a9fd87b8b
Andres Barro <andres@localhost.localdomain>
2017-05-05 15:08:36 -0500

    Create queue, slight changes to game

--- game.cpp
+++ game.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <"queue.h">
+
 using namespace std;
 
 class Game {
@@ -7,9 +9,16 @@ public:
   string name;
   string developer;
   int year;
+  RedBlackTree adj;
   Game(string name, string developer, int year) {
     this->name = name;
     this->developer = developer;
     this->year = year;
+    this->adj = new RedBlackTree();
   }
+  void add_edge(Game *end){
+    Edge *e = new edge(end, 0/* Call method to get closeness */);
+    this->adj.insert(e, e->weight);
+  }
+
 };
---
+++ queue.h
@@ -0,0 +1,67 @@
+#ifndef __QUEUE_H_INCLUDED__
+#define __QUEUE_H_INCLUDED__
+
+template <typename E> class Queue{
+    private:
+        struct node{
+            E *e;
+            node *next;
+        }
+        int size;
+        node *top;
+        node *bot;
+    public:
+        Queue(int arr_size);
+        ~Queue();
+        bool is_empty();
+        int size();
+        void enqueue();
+        E * dequeue();
+};
+
+template <typename E>
+Queue<E>::Queue(int arr_size){
+    this->size = 0;
+    this->bot = this->top = NULL;
+}
+template <typename E>
+Queue<E>::~Queue(){
+    for(node *tmp = bot, *next = NULL; tmp != NULL; tmp = next){
+        next = tmp->next;
+        delete tmp;
+    }
+}
+template <typename E>
+bool Queue<E>::is_empty(){
+    return this->size == 0;
+}
+template <typename E>
+void Queue<E>::size(){
+    return this->size;
+}
+template <typename E>
+void Queue<E>::enqueue(E *value){
+    node *n = new node;
+    node->next = NULL;
+    node->e = value;
+    if(this->top != NULL)
+        this->top->next = n;
+    else
+        this->bot = n;
+    this->top = n;
+    ++this->size;
+}
+template <typename E>
+E * Queue<E>::dequeue(E *e){
+    if(this->is_empty()) return NULL;
+    E *e = this->bot->e;
+    node to_del = this->bot;
+    this->bot = this->bot->next;
+    if(this->bot == NULL)
+        this->top = NULL;
+    delete to_del;
+    --this->size;
+    return e;
+}
+
+#endif
