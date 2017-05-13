172d3cce4acbad32eeee38c6a65635b3ba481063
fornesarturo <fornesarturo@gmail.com>
2017-05-12 19:56:40 -0500

    Added line for forward declare

---
+++ cowsay.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout << "______________________" << endl;
+cout << " Welcome to GIT GUD " << endl;
+cout << "----------------------" << endl;
+cout << "   \\  ^__^" << endl;
+cout << "    \\ (oo)\\_______ " << endl;
+cout << "      (__)\\       )\\/\\" << endl;
+cout << "	  ||----w |" << endl;
+cout << "	  ||     ||" << endl;
+return 0;
+}
